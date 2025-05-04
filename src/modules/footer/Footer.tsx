776240ae76939f1796373b7cfaff76a0537a6a6c
Jhony Vidal <jhonylopez147@hotmail.com>
2025-05-04 17:13:06 -0500

    update href contact and home

--- src/modules/footer/Footer.tsx
+++ src/modules/footer/Footer.tsx
@@ -1,18 +1,28 @@
 const Footer = () => {
-
-    return (
-        <footer className="fixed px-[var(--horizontal-padding)] w-full h-20 flex bottom-0 left-0">
-            <div className="w-1/2">
-                <p className="text-2xl text-white"><span className="text-[var(--cyan)]">[futurebit]</span> Experimental TECH, yet essential_</p>
-            </div>
-            <div className="w-1/2 flex justify-end items-start">
-                <button className="ml-10 px-5 py-2 text-white text-2xl rounded-2xl border border-white cursor-pointer hover:bg-white/20">PORTFOLIO</button>
-                <button className="ml-10 px-5 py-2 text-white text-2xl rounded-2xl border border-white cursor-pointer hover:bg-white/20">PACKAGES</button>
-                <button className="ml-10 px-5 py-2 text-white text-2xl rounded-2xl border border-white cursor-pointer hover:bg-white/20">CONTACT</button>
-            </div>
-        </footer>
-    );
-
-}
+  return (
+    <footer className="fixed px-[var(--horizontal-padding)] w-full h-20 flex bottom-0 left-0">
+      <div className="w-1/2">
+        <p className="text-2xl text-white">
+          <span className="text-[var(--cyan)]">[futurebit]</span> Experimental
+          TECH, yet essential_
+        </p>
+      </div>
+      <div className="w-1/2 flex justify-end items-start">
+        <button className="ml-10 px-5 py-2 text-white text-2xl rounded-2xl border border-white cursor-pointer hover:bg-white/20">
+          PORTFOLIO
+        </button>
+        <button className="ml-10 px-5 py-2 text-white text-2xl rounded-2xl border border-white cursor-pointer hover:bg-white/20">
+          PACKAGES
+        </button>
+        <a
+          href="/contact"
+          className="ml-10 px-5 py-2 text-white text-2xl rounded-2xl border border-white cursor-pointer hover:bg-white/20"
+        >
+          CONTACT
+        </a>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/modules/navbar/Navbar.tsx
+++ src/modules/navbar/Navbar.tsx
@@ -1,11 +1,15 @@
 const Navbar = () => {
-
-    return (
-        <nav className="fixed pl-[var(--horizontal-padding)] w-full h-20 top-0 left-0 flex items-center z-1">
-            <img src="https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/0c5de999-6de4-4db0-ffd3-2f84da547000/public" alt="Alerta labs png" />
-        </nav>
-    );
-
-}
+  return (
+    <nav className="fixed pl-[var(--horizontal-padding)] w-full h-20 top-0 left-0 flex items-center z-1">
+      <a href="/">
+        <img
+          src="https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/0c5de999-6de4-4db0-ffd3-2f84da547000/public"
+          alt="Alerta labs png"
+          className="cursor-pointer"
+        />
+      </a>
+    </nav>
+  );
+};
 
 export default Navbar;
