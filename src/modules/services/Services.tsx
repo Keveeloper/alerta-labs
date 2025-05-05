49af5034b1c16452020fc6f2fd70beaad32ed7a4
Kevind Ospina <kevind171819@gmail.com>
2025-05-05 17:31:24 -0500

    Service page design completed

--- src/modules/services/Services.tsx
+++ src/modules/services/Services.tsx
@@ -1,5 +1,19 @@
-const Services = () => {
+import { Swiper, SwiperSlide } from 'swiper/react';
+import "swiper/css";
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
 
+const UXxplosion = [
+    {
+        image: "https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/4450f5f2-5941-409d-f4f8-6e38dee44800/public",
+        title: "UX / UI",
+    },
+    {
+        image: "https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/402caacf-480f-4e5c-6e26-e87ad05e5b00/public",
+        title: "Assets",
+    },
+]
+const Services = () => {
     return (
         <div className="px-[var(--horizontal-padding)] w-full h-screen flex justify-center items-center">
             <div className="w-full h-[80%] flex">
@@ -9,7 +23,35 @@ const Services = () => {
                         <button className="p-5 border rounded-2xl border-white">{'<<'}</button>
                     </div>
                     <div className="w-full h-[85%] border border-white rounded-2xl">
-
+                        <Swiper
+                            className='alerta-labs-swiper w-full h-full'
+                            navigation={true} 
+                            modules={[Navigation]}
+                            spaceBetween={50}
+                            slidesPerView={1}
+                            onSlideChange={() => console.log('slide change')}
+                            onSwiper={(swiper) => console.log(swiper)}
+                        >
+                            {UXxplosion.map((item, index) => (
+                                <SwiperSlide className='w-full h-full'>
+                                    <div className='w-full h-[70%] flex justify-center items-center'>
+                                        <img src={item.image} alt="" />
+                                    </div>
+                                    <hr className='mx-10 text-white'/>
+                                    <div className='w-full h-[30%] flex justify-center items-center'>
+                                        <h2 
+                                            className='w-[80%] text-white text-6xl'
+                                            style={{fontFamily: 'Bebas Neue'}}
+                                        >
+                                            {item.title}
+                                        </h2>
+                                    </div>
+                                </SwiperSlide>
+                            ))}
+                            {/* <SwiperSlide className='w-full h-full'>Slide 2</SwiperSlide>
+                            <SwiperSlide className='w-full h-full'>Slide 3</SwiperSlide>
+                            <SwiperSlide className='w-full h-full'>Slide 4</SwiperSlide> */}
+                        </Swiper>
                     </div>
                 </div>
                 <div className="w-[65%] h-full flex justify-center items-center">
