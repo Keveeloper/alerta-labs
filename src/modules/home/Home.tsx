fef0817b18f20f591fbb601c3848da0f946bd3f6
Profesional digital <profesionaldigital@impormedical.com.co>
2025-05-13 17:37:29 -0500

    Home highlighted completed

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -8,6 +8,8 @@ const Home = () => {
     const [stationHighlighted, setStationHighlighted] = useState<string>('1cd060bc-6e36-4286-3d92-234765af0300');
     const [cristalHighlighted, setCristalHighlighted] = useState<string>('c86ffcab-84fb-4f9c-401b-7eb5d2a69900');
     const [nebulaHighlighted, setNebulaHighlighted] = useState<string>('642af0a5-a5a3-4134-fcf7-c785a9f01500');
+    const [satelliteHighlighted, setSatelliteHighlighted] = useState<string>('4667d586-b939-47bb-4a00-312799925e00');
+    const [asteroidHighlighted, setAsteroidHighlighted] = useState<string>('96444b46-5e0f-4433-4dc0-2f1abc3c1600');
 
     const handleHover = (text: string) => {
         setTextHovered(text);
@@ -21,6 +23,12 @@ const Home = () => {
             case '[UXxplosion] Cosmic clarity in pixels_':
                 setNebulaHighlighted('3745c65f-ceda-4557-f643-11551e716000');
                 break;
+            case '[update base] App-building in zero-G_':
+                setSatelliteHighlighted('9d447971-ee1b-46d8-6cd0-48bc1c688900');
+                break;
+            case '[brand boulder] Built to carry identities_':
+                setAsteroidHighlighted('e5f7495d-19fa-46b8-9a2f-098f3a727300');
+                break;
                 default:
                     break;
                 }
@@ -30,40 +38,56 @@ const Home = () => {
         setStationHighlighted('1cd060bc-6e36-4286-3d92-234765af0300');
         setCristalHighlighted('c86ffcab-84fb-4f9c-401b-7eb5d2a69900');
         setNebulaHighlighted('642af0a5-a5a3-4134-fcf7-c785a9f01500');
+        setSatelliteHighlighted('4667d586-b939-47bb-4a00-312799925e00');
+        setAsteroidHighlighted('96444b46-5e0f-4433-4dc0-2f1abc3c1600');
     }
 
     return (
         <main className="w-full h-lvh">
-            <img 
-                src="https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/47a4e8a6-c2b3-429f-c834-86343375a200/public" alt="solar system background" 
-                className="fixed w-full h-full left-1/2 -translate-x-1/2 object-cover mix-blend-screen"
-            />
-            <img 
-                src="https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/09cb6e8c-0893-4171-0e4a-dce1538b5d00/public" alt="astronaut alerta labs" 
-                className="absolute bottom-[8%] left-1/2 -translate-x-1/2"
-            />
-            <img 
-                src={`https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/${cristalHighlighted}/public`} alt="Glass diamonds" 
-                className="absolute w-[213px] top-[8%] right-[20%] -translate-x-1/2"
-                onMouseEnter={() => handleHover("[futurebit] Experimental TECH, yet essential_")}
-                onMouseLeave={handleMouseLeave}
-            />
-            <img 
-                src="https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/2a615b82-df16-482f-e78c-571588b7f600/public" alt="Asteroid" 
-                className="absolute top-[20%] right-0 -translate-x-1/2"
-                onMouseEnter={() => handleHover("[brand boulder] Built to carry identities_")}
+            <Link to={'/services'}>
+                <img 
+                    src="https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/47a4e8a6-c2b3-429f-c834-86343375a200/public" alt="solar system background" 
+                    className="fixed w-full h-full left-1/2 -translate-x-1/2 object-cover mix-blend-screen"
                 />
-            <img 
-                src={`https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/${stationHighlighted}/public`} alt="Station" 
-                className="absolute w-[320px] bottom-[15%] right-[5%]"
-                onMouseEnter={() => handleHover("[codecrumb] NEW tech, new tricks_")}
-                onMouseLeave={handleMouseLeave}
-            />
-            <img 
-                src="https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/973bfea9-3bb4-408f-1b85-0110a76cb300/public" alt="Satellite" 
-                className="absolute w-[400px] bottom-[5%] left-0 z-1"
-                onMouseEnter={() => handleHover("[update base] App-building in zero-G_")}
+            </Link>
+            <Link to={'/services'}>
+                <img 
+                    src="https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/09cb6e8c-0893-4171-0e4a-dce1538b5d00/public" alt="astronaut alerta labs" 
+                    className="absolute bottom-[8%] left-1/2 -translate-x-1/2"
                 />
+            </Link>
+            <Link to={'/services'}>
+                <img 
+                    src={`https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/${cristalHighlighted}/public`} alt="Glass diamonds" 
+                    className="absolute w-[213px] top-[8%] right-[20%] -translate-x-1/2"
+                    onMouseEnter={() => handleHover("[futurebit] Experimental TECH, yet essential_")}
+                    onMouseLeave={handleMouseLeave}
+                />
+            </Link>
+            <Link to={'/services'}>
+                <img 
+                    src={`https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/${asteroidHighlighted}/public`} alt="Asteroid" 
+                    className="absolute w-[228px] top-[20%] right-0 -translate-x-1/2"
+                    onMouseEnter={() => handleHover("[brand boulder] Built to carry identities_")}
+                    onMouseLeave={handleMouseLeave}
+                />
+            </Link>
+            <Link to={'/services'}>
+                <img 
+                    src={`https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/${stationHighlighted}/public`} alt="Station" 
+                    className="absolute w-[320px] bottom-[15%] right-[5%]"
+                    onMouseEnter={() => handleHover("[codecrumb] NEW tech, new tricks_")}
+                    onMouseLeave={handleMouseLeave}
+                />
+            </Link>
+            <Link to={'/services'}>
+                <img 
+                    src={`https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/${satelliteHighlighted}/public`} alt="Satellite" 
+                    className="absolute w-[400px] bottom-[5%] left-0 z-1"
+                    onMouseEnter={() => handleHover("[update base] App-building in zero-G_")}
+                    onMouseLeave={handleMouseLeave}
+                />
+            </Link>
             <Link to={'/services'}>
                 <img 
                     src={`https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/${nebulaHighlighted}/public`} alt="Nebula" 
