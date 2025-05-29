6c2a6507d07858d5b5a21f4d966d2643ef02a022
Profesional digital <profesionaldigital@impormedical.com.co>
2025-05-29 09:44:41 -0500

    Local changes

--- src/modules/footer/Footer.tsx
+++ src/modules/footer/Footer.tsx
@@ -10,26 +10,26 @@ const Footer = () => {
 
   return (
     <footer className="fixed px-[var(--horizontal-padding)] w-full h-20 flex bottom-0 left-0 z-2">
-      <div className="w-1/2">
-        <p className="text-2xl text-white font-[Exan]">
+      <div className="w-1/2 flex items-center">
+        <p className="text-xl text-white font-[Exan]">
           <span className="text-[var(--cyan)]">{textHoovered.split(']')[0]}{']'}</span>
           {textHoovered.split(']')[1]}
         </p>
       </div>
       <div className="w-1/2 flex justify-end items-start">
-        <button className="ml-10 px-5 py-2 text-white text-2xl rounded-2xl border border-white cursor-pointer font-[Exan] hover:bg-white/20">
+        <button className="w-[175px] text-center ml-10 px-5 py-2 text-white text-2xl rounded-2xl border border-white cursor-pointer font-[Exan] hover:bg-white/20">
           Portfolio
         </button>
         <a
           // href="/packages"
           onClick={() => setIsModalOpen(true)}
-          className="ml-10 px-5 py-2 text-white text-2xl rounded-2xl border border-white cursor-pointer font-[Exan] hover:bg-white/20"
+          className="w-[175px] text-center ml-10 px-5 py-2 text-white text-2xl rounded-2xl border border-white cursor-pointer font-[Exan] hover:bg-white/20"
         >
           Packages
         </a>
         <a
           href="/contact"
-          className="ml-10 px-5 py-2 text-white text-2xl rounded-2xl border border-white cursor-pointer font-[Exan] hover:bg-white/20"
+          className="w-[175px] text-center ml-10 px-5 py-2 text-white text-2xl rounded-2xl border border-white cursor-pointer font-[Exan] hover:bg-white/20"
         >
           Contact
         </a>
