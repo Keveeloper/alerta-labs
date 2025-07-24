e68fc4b72354d37a3b54231188b357b1c1f524af
Profesional digital <profesionaldigital@impormedical.com.co>
2025-07-24 10:37:09 -0500

    Astronaut contact fixed and team sort new logo

--- src/modules/navbar/Navbar.tsx
+++ src/modules/navbar/Navbar.tsx
@@ -3,9 +3,9 @@ const Navbar = () => {
     <nav className="fixed pl-[var(--horizontal-padding)] w-full h-20 top-0 left-0 flex items-center z-2">
       <a href="/">
         <img
-          src="https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/0c5de999-6de4-4db0-ffd3-2f84da547000/public"
-          alt="Alerta labs png"
-          className="cursor-pointer"
+          src="https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/3a102297-c7f1-4c13-82a7-84d2623d1b00/public"
+          alt="Alerta labs logo png"
+          className="w-[280px] cursor-pointer"
         />
       </a>
     </nav>
